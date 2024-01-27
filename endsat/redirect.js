5cd8b528fd07c05bf68668ed6b8bb31cc16ba2d2
nathanpuls <natepuls@gmail.com>
2024-01-27 10:31:48 -0600

    quack

---
+++ endsat/redirect.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function () {
+    // Get the value of the 'name' parameter from the URL
+    var urlParams = new URLSearchParams(window.location.search);
+    var nameWithDot = urlParams.get('name');
+
+    // Check if the 'name' parameter is present and contains a dot
+    if (nameWithDot && nameWithDot.includes('.') && !window.location.href.includes('https://wyr.es/')) {
+      // Extract the name after the dot
+      var name = nameWithDot.split('.')[1];
+
+      // Log the extracted name to the console
+      console.log('Extracted name:', name);
+
+      // Redirect to the specified URL
+      window.location.href = 'https://wyr.es/' + name;
+    }
+  });
